ae82a99941ae147859ccf8397d648b5d90016da7
designlab87 <sethwatson1976@gmail.com>
2022-10-03 00:38:02 -0500

    Start the meal order app
    Built out the basic components and styles

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+
+import MealsSummary from './MealsSummary';
+import AvailableSummary from './AvailableMeals';
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableSummary />
+    </Fragment>
+  );
+};
+
+export default Meals;
